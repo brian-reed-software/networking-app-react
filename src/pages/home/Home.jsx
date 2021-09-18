f0fb92e1ef105dd07e5196189e5b049d89ce32c3
Bryan Reed <brian.reed.software@outlook.com>
2021-09-18 11:57:10 -0700

    updated styling

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,17 +2,18 @@ import Header from "../../componants/header/Header";
 import Sidebar from "../../componants/sidebar/Sidebar";
 import Feed from "../../componants/feed/Feed";
 import Rightbar from "../../componants/rightbar/Rightbar";
-
+import "./Home.css"
 
 export default function Home() {
 
     return (
         <>
         <Header/>
-        <div className="homeContainer"></div>
+        <div className="homeContainer">
         <Sidebar/>
         <Feed/>
         <Rightbar/>
+        </div>
         </>
     );
 }
