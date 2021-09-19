4736c6115e95e5961a7e48cb94116040037ca8c7
Bryan Reed <brian.reed.software@outlook.com>
2021-09-18 21:52:30 -0700

    updated styling

--- src/componants/share/Share.jsx
+++ src/componants/share/Share.jsx
@@ -1,16 +1,20 @@
 import "./Share.css"
-import { PermMedia } from "@material-ui/icons";
+import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
 
 
 export default function Share() {
     return (
         <div className="shareContainer">
         <div className="shareWrapper">
+            <div className="shareUserInfoWrapper">
             <div className="shareUserInfo">
             <div className="userName">
             <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <div className="shareProfileText">
                 Betty
-                </div>
+            </div>
+            </div>
+            </div>
             <div className="shareBar">
             <input
             placeholder="What would you like to share Betty?"
@@ -19,16 +23,41 @@ export default function Share() {
             />
 
             </div>
+            <div className="shareButton">
+                Send
+            </div>
             </div>
             <hr className="shareHr"/>
+
             <div className="shareOptions">
-                <PermMedia className="shareIcons"/>
                 <div className="shareOption">
+                <PermMedia className="shareIcons"/>
                     <span className="shareOptionText">
                         Upload <br/>
                     </span>
                 </div>
+                <div className="shareOption">
+                <Label className="shareIcons"/>
+                    <span className="shareOptionText">
+                        Tag <br/>
+                    </span>
+                </div>
+                <div className="shareOption">
+                <Room className="shareIcons"/>
+                    <span className="shareOptionText">
+                        Location <br/>
+                    </span>
+                </div>
+                <div className="shareOption">
+                <EmojiEmotions className="shareIcons"/>
+                    <span className="shareOptionText">
+                        Emoji <br/>
+                    </span>
+                </div>
+
             </div>
+
+
         </div>
         
         </div>
