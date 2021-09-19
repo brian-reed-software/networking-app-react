9498cd8f814b80c3536309c3f5ae55efa7f90ef3
Bryan Reed <brian.reed.software@outlook.com>
2021-09-18 20:38:01 -0700

    updated styling

--- src/componants/share/Share.jsx
+++ src/componants/share/Share.jsx
@@ -31,32 +31,7 @@ export default function Share() {
                 </div>
             </div>
         </div>
-        <div className="shareWrapper">
-            <div className="shareUserInfo">
-            <div className="userName">
-            <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
-                Betty
-                </div>
-            <div className="shareBar">
-            <input
-            placeholder="What would you like to share Betty?"
-            className="shareInput"
-            type="text"
-            />
-
-            </div>
-            </div>
-            <hr className="shareHr"/>
-            <div className="shareOptions">
-                <PermMedia className="shareIcons"/>
-                <div className="shareOption">
-                    <span className="shareOptionText">
-                        Upload <br/>
-                        Photo / Video
-                    </span>
-                </div>
-            </div>
-        </div>
+        
         </div>
     );
 }
