97ebdd8e9e6c004d39447d885bb043650b125e60
Bryan Reed <brian.reed.software@outlook.com>
2021-09-20 11:56:47 -0700

    updated styling

--- src/componants/post/Post.jsx
+++ src/componants/post/Post.jsx
@@ -5,22 +5,15 @@ import { PermMedia, Label, Room, EmojiEmotions } from "@material-ui/icons";
 export default function Post() {
     return (
         <div className="postContainer">
+        
         <div className="postWrapper">
-
         <div className="postUserWrapper">
-
-                
                     <span className="postName">
-                    <PermMedia className="postIcons"/> Image Name posted min ago
+                    <PermMedia className="postIcons"/> 
+                    Image Name posted min 20 ago
                     </span>
                         <img className="postImg" alt="" src="/assets/post/8.jpeg" ></img>
-                <div className="postStoryWrapper">
-                <textarea
-                     className="postStory">   
-                        this Story
-                        this story
-                        this story
-                    </textarea>
+                <div className="postInfoWrapper">
 
                 <div className="postLocEmoji">
 
@@ -67,11 +60,17 @@ export default function Post() {
                 </span> 
                 </div>
 
-
-
-                </div>
+        </div>
                
+            <div className="postStoryWrapper">
+                <textarea
+                     className="postStory">   
+                        this Story
+                        this story
+                        this story
+                    </textarea>
             </div>
+    </div>
 
 
             
@@ -97,10 +96,371 @@ export default function Post() {
             </div> */}
             </div>
             {/* <hr className="shareHr"/> */}
+        </div>
+        <div className="postWrapper">
+        <div className="postUserWrapper">
+                    <span className="postName">
+                    <PermMedia className="postIcons"/> 
+                    Image Name posted min 20 ago
+                    </span>
+                        <img className="postImg" alt="" src="/assets/post/8.jpeg" ></img>
+                <div className="postInfoWrapper">
+
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocation">
+                <Room className="postIcons"/>
+                    <span className="postOptionText">
+                        Somewhere
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        :\
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionLikes">
+                        Likes
+                        <span className="likesCounter">
+                            15
+                        </span>
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        Hearts
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postOptionComments">
+                <EmojiEmotions className="postIcons"/>
+                <span className="commentCounter">
+                    20
+                </span>
+                <span className="postOptionText">
+                    Comments
+                </span> 
+                </div>
+
+        </div>
+               
+            <div className="postStoryWrapper">
+                <textarea
+                     className="postStory">   
+                        this Story
+                        this story
+                        this story
+                    </textarea>
+            </div>
+    </div>
 
 
+            
+            <div className="postUserInfoWrapper">
+            <div className="postUserInfo">
+            <div className="postNameText">
+            <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <div className="postProfileText">
+                Betty
+            </div>
+            </div>
+            </div>
+            <div className="postBar">
+            {/* <input
+            placeholder="What would you like to share Betty?"
+            className="postInput"
+            type="text"
+            /> */}
+            Posted at 1:23 PM PST
+            </div>
+            {/* <div className="postButton">
+                Send
+            </div> */}
+            </div>
+            {/* <hr className="shareHr"/> */}
+        </div>
+        <div className="postWrapper">
+        <div className="postUserWrapper">
+                    <span className="postName">
+                    <PermMedia className="postIcons"/> 
+                    Image Name posted min 20 ago
+                    </span>
+                        <img className="postImg" alt="" src="/assets/post/8.jpeg" ></img>
+                <div className="postInfoWrapper">
+
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocation">
+                <Room className="postIcons"/>
+                    <span className="postOptionText">
+                        Somewhere
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        :\
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionLikes">
+                        Likes
+                        <span className="likesCounter">
+                            15
+                        </span>
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        Hearts
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postOptionComments">
+                <EmojiEmotions className="postIcons"/>
+                <span className="commentCounter">
+                    20
+                </span>
+                <span className="postOptionText">
+                    Comments
+                </span> 
+                </div>
+
+        </div>
+               
+            <div className="postStoryWrapper">
+                <textarea
+                     className="postStory">   
+                        this Story
+                        this story
+                        this story
+                    </textarea>
+            </div>
+    </div>
 
 
+            
+            <div className="postUserInfoWrapper">
+            <div className="postUserInfo">
+            <div className="postNameText">
+            <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <div className="postProfileText">
+                Betty
+            </div>
+            </div>
+            </div>
+            <div className="postBar">
+            {/* <input
+            placeholder="What would you like to share Betty?"
+            className="postInput"
+            type="text"
+            /> */}
+            Posted at 1:23 PM PST
+            </div>
+            {/* <div className="postButton">
+                Send
+            </div> */}
+            </div>
+            {/* <hr className="shareHr"/> */}
+        </div>
+        <div className="postWrapper">
+        <div className="postUserWrapper">
+                    <span className="postName">
+                    <PermMedia className="postIcons"/> 
+                    Image Name posted min 20 ago
+                    </span>
+                        <img className="postImg" alt="" src="/assets/post/8.jpeg" ></img>
+                <div className="postInfoWrapper">
+
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocation">
+                <Room className="postIcons"/>
+                    <span className="postOptionText">
+                        Somewhere
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        :\
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionLikes">
+                        Likes
+                        <span className="likesCounter">
+                            15
+                        </span>
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        Hearts
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postOptionComments">
+                <EmojiEmotions className="postIcons"/>
+                <span className="commentCounter">
+                    20
+                </span>
+                <span className="postOptionText">
+                    Comments
+                </span> 
+                </div>
+
+        </div>
+               
+            <div className="postStoryWrapper">
+                <textarea
+                     className="postStory">   
+                        this Story
+                        this story
+                        this story
+                    </textarea>
+            </div>
+    </div>
+
+
+            
+            <div className="postUserInfoWrapper">
+            <div className="postUserInfo">
+            <div className="postNameText">
+            <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <div className="postProfileText">
+                Betty
+            </div>
+            </div>
+            </div>
+            <div className="postBar">
+            {/* <input
+            placeholder="What would you like to share Betty?"
+            className="postInput"
+            type="text"
+            /> */}
+            Posted at 1:23 PM PST
+            </div>
+            {/* <div className="postButton">
+                Send
+            </div> */}
+            </div>
+            {/* <hr className="shareHr"/> */}
+        </div>
+                
+        <div className="postWrapper">
+        <div className="postUserWrapper">
+                    <span className="postName">
+                    <PermMedia className="postIcons"/> 
+                    Image Name posted min 20 ago
+                    </span>
+                        <img className="postImg" alt="" src="/assets/post/8.jpeg" ></img>
+                <div className="postInfoWrapper">
+
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocation">
+                <Room className="postIcons"/>
+                    <span className="postOptionText">
+                        Somewhere
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        :\
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postLocEmoji">
+
+                <div className="postOptionLocEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionLikes">
+                        Likes
+                        <span className="likesCounter">
+                            15
+                        </span>
+                    </span>
+                </div>
+                <div className="postOptionEmoji">
+                <EmojiEmotions className="postIcons"/>
+                    <span className="postOptionText">
+                        Hearts
+                    </span>
+ 
+                </div>
+                </div>
+                <div className="postOptionComments">
+                <EmojiEmotions className="postIcons"/>
+                <span className="commentCounter">
+                    20
+                </span>
+                <span className="postOptionText">
+                    Comments
+                </span> 
+                </div>
+
+        </div>
+               
+            <div className="postStoryWrapper">
+                <textarea
+                     className="postStory">   
+                        this Story
+                        this story
+                        this story
+                    </textarea>
+            </div>
+    </div>
+
+
+            
+            <div className="postUserInfoWrapper">
+            <div className="postUserInfo">
+            <div className="postNameText">
+            <img className="postProfileImg" src="/assets/person/1.jpeg" alt="" />
+            <div className="postProfileText">
+                Betty
+            </div>
+            </div>
+            </div>
+            <div className="postBar">
+            {/* <input
+            placeholder="What would you like to share Betty?"
+            className="postInput"
+            type="text"
+            /> */}
+            Posted at 1:23 PM PST
+            </div>
+            {/* <div className="postButton">
+                Send
+            </div> */}
+            </div>
+            {/* <hr className="shareHr"/> */}
         </div>
         
         </div>
--- src/componants/rightbar/Rightbar.jsx
+++ src/componants/rightbar/Rightbar.jsx
@@ -1,12 +1,118 @@
 import "./Rightbar.css"
-import { Search, Person, Chat, Notifications } from "@material-ui/icons";
+import { RssFeed, Chat, OndemandVideo, Group, Bookmark, HelpOutline, Event, School  } from "@material-ui/icons";
 
 
-export default function Header() {
+export default function Rightbar() {
     return (
         <div className="rightbarContainer">
         <div className="rightbarWrapper">
-            Rightbar
+            <ul className="rightbarList">
+            <li className="rightbarListItem">
+            <img className="birthdayImg" 
+            src="/assets/gift.png" 
+            alt=""/>
+                    <span className="rightbarListItemText">
+                        
+                        <b>Rajs</b> and 3 others have <b>birthdays</b> today!
+                    </span>
+
+                </li>
+                <li className="rightbarListItem">
+                    <span className="rightbardFriendOnline">
+                    Friends Online
+
+                    </span>
+                    <span className="rightbarFriend">
+                        <div className="rightbarProfileImgContainer">
+                        <img className="rightbarProfileImg" 
+                        src="/assets/person/4.jpeg" 
+                        alt=""/>
+                        <div className="rightbarProfileImgName">
+                            Leo
+                        </div>
+                        </div>
+                    </span>
+                </li>
+                <li className="rightbarListItem">
+                    <OndemandVideo className="rightbarIcon" />
+                    <span className="rightbarListItemText">
+                        Videos
+                    </span>
+                </li>
+                <li className="rightbarListItem">
+                    <Group className="rightbarIcon" />
+                    <span className="rightbarListItemText">
+                        Groups
+                    </span>
+                </li>
+                <li className="rightbarListItem">
+                    <Bookmark className="rightbarIcon" />
+                    <span className="rightbarListItemText">
+                        Bookmarks
+                    </span>
+                </li>
+                <li className="rightbarListItem">
+                    <HelpOutline className="rightbarIcon" />
+                    <span className="rightbarListItemText">
+                        Help
+                    </span>
+                </li>
+                <li className="rightbarListItem">
+                    <Event className="rightbarIcon" />
+                    <span className="rightbarListItemText">
+                        Events
+                    </span>
+                </li>
+                <li className="rightbarListItem">
+                    <School className="rightbarIcon" />
+                    <span className="rightbarListItemText">
+                        School
+                    </span>
+                </li>
+            </ul>
+            <button className="rightbarButton">
+                Show More
+            </button>
+            <hr className="rightbarHr"/>
+            <div className="rightbarFriendWrapper">
+            <li className="rightbarFriend">
+                <img className="rightbarFriendImg" src="/assets/person/2.jpeg"/>
+                <span className="rightbarFriendName">
+                    Friend Name
+                </span>
+            </li>
+            <li className="rightbarFriend">
+                <img className="rightbarFriendImg" src="/assets/person/2.jpeg"/>
+                <span className="rightbarFriendName">
+                    Friend Name
+                </span>
+            </li>
+            <li className="rightbarFriend">
+                <img className="rightbarFriendImg" src="/assets/person/2.jpeg"/>
+                <span className="rightbarFriendName">
+                    Friend Name
+                </span>
+            </li>
+            <li className="rightbarFriend">
+                <img className="rightbarFriendImg" src="/assets/person/2.jpeg"/>
+                <span className="rightbarFriendName">
+                    Friend Name
+                </span>
+            </li>
+            <li className="rightbarFriend">
+                <img className="rightbarFriendImg" src="/assets/person/2.jpeg"/>
+                <span className="rightbarFriendName">
+                    Friend Name
+                </span>
+            </li>
+            <li className="rightbarFriend">
+                <img className="rightbarFriendImg" src="/assets/person/2.jpeg"/>
+                <span className="rightbarFriendName">
+                    Friend Name
+                </span>
+            </li>
+            </div>
+            
         </div>
         </div>
     );
