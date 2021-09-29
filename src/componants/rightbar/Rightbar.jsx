615932b74cee6b3f1f764f39d228695d38db150a
Bryan Reed <brian.reed.software@outlook.com>
2021-09-28 20:42:45 -0700

    added login and register

--- src/componants/rightbar/Rightbar.jsx
+++ src/componants/rightbar/Rightbar.jsx
@@ -1,101 +1,105 @@
-import "./Rightbar.css"
-import {Bookmark, HelpOutline, Event, School  } from "@material-ui/icons";
-import Online from "../../componants/online/Online";
-import {Users} from "../../Data.js"
-export default function Rightbar({profile}) {
-    
-        const HomeRightbar = () =>{
-            return(
-                <>
-                     <ul className="rightbarList">
-            <li className="rightbarListItem">
+import "./Rightbar.css";
+import { Users } from "../../Data";
+import Online from "../online/Online";
 
-                    <span className="rightbarBirthdayOnline">
-                    <img alt ="" className="birthdayImg" 
-            src="/assets/gift.png" 
-            alt=""/>
-                        <b>Rajs</b> and 3 others have <b>birthdays</b> today!
-                    </span>
-
-                </li>
-                <li className="rightbarFriendsOnlineCount">
-                        Friends Online
-                    <span className="rightbardFriendOnline">
-                    </span>
-                </li>
-
-                <div className="rightbarFriendList">
-
-                    {Users.map((u) =>
-                    <Online key={u.id} user={u} />
-                        )}
-
-                </div>
-
-                <li className="rightbarListItem">
-                </li>
-                
-            </ul>
-            <button className="rightbarButton">
-                Show More
-            </button>
-            <hr className="rightbarHr"/>
-
-                </>
-            )
-        }
-
-const ProfileRightbar = () => {
-    return(
-    <>
-    <h4 className="rightbarTitle">User Information</h4>
-    <div className="rightbarInfo">
-        
-    <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">
-                City:
-            </span>
-            <span className="rightbarInfoValue">
-                PTRLD
-            </span>            
+export default function Rightbar({ profile }) {
+  const HomeRightbar = () => {
+    return (
+      <>
+        <div className="birthdayContainer">
+          <img className="birthdayImg" src="assets/gift.png" alt="" />
+          <span className="birthdayText">
+            <b>Pola Foster</b> and <b>3 other friends</b> have a birhday today.
+          </span>
         </div>
-
-        <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">
-                From:
-            </span>
-            <span className="rightbarInfoValue">
-                PTRLD
-            </span>            
-        </div>
-
-        <div className="rightbarInfoItem">
-            <span className="rightbarInfoKey">
-                Bio:
-            </span>
-            <span className="rightbarInfoValue">
-                Working
-            </span>            
-        </div>
-        <h4 className="rightbarTitle">User Friends</h4>
-        <div className="rightbarFollowings">
-            <div className="rightbarFollowing">
-                <img src="assets/people/1.jpeg" alt="" className="rightbarFollowingImg" />
-                <div className="span rightbarFollowingName">
-                    Johnnnnnn
-                </div>
-            </div>
-        </div>
-
-    </div>
-    </>)
-}
-        return (
-
-        <div className="rightbarContainer">
-        <div className="rightbarWrapper">
-       <ProfileRightbar/>  
-        </div>
-        </div>
+        <img className="rightbarAd" src="assets/ad.png" alt="" />
+        <h4 className="rightbarTitle">Online Friends</h4>
+        <ul className="rightbarFriendList">
+          {Users.map((u) => (
+            <Online key={u.id} user={u} />
+          ))}
+        </ul>
+      </>
     );
+  };
+
+  const ProfileRightbar = () => {
+    return (
+      <>
+        <h4 className="rightbarTitle">User information</h4>
+        <div className="rightbarInfo">
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">City:</span>
+            <span className="rightbarInfoValue">New York</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">From:</span>
+            <span className="rightbarInfoValue">Madrid</span>
+          </div>
+          <div className="rightbarInfoItem">
+            <span className="rightbarInfoKey">Relationship:</span>
+            <span className="rightbarInfoValue">Single</span>
+          </div>
+        </div>
+        <h4 className="rightbarTitle">User friends</h4>
+        <div className="rightbarFollowings">
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/1.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/2.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/3.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/4.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/5.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+          <div className="rightbarFollowing">
+            <img
+              src="assets/person/6.jpeg"
+              alt=""
+              className="rightbarFollowingImg"
+            />
+            <span className="rightbarFollowingName">John Carter</span>
+          </div>
+        </div>
+      </>
+    );
+  };
+  return (
+    <div className="rightbar">
+      <div className="rightbarWrapper">
+        {profile ? <ProfileRightbar /> : <HomeRightbar />}
+      </div>
+    </div>
+  );
 }
