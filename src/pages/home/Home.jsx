9f4fa2547899c336178b0ad3a66cf77dbf1d7ff4
Bryan Reed <brian.reed.software@outlook.com>
2021-10-01 00:16:17 -0700

    updated case on css file name to try and fix netlify issue

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -2,7 +2,7 @@ import Header from "../../components/header/Header";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
-import "./Home.css"
+import "./home.css"
 
 export default function Home() {
 
