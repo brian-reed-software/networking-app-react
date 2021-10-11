827a7fee24f30348e853682d9930918283531a7e
Bryan Reed <brian.reed.software@outlook.com>
2021-10-10 17:12:18 -0700

    trying to fix cors issue

--- src/apiCalls.js
+++ src/apiCalls.js
@@ -1,12 +1,19 @@
 import axios from "axios";
 
 export const loginCall = async (userCredential, dispatch) => {
-  dispatch({ type: "LOGIN_START" });
+  dispatch({
+    type: "LOGIN_START"
+  });
   try {
-    const res = await axios.post("https://friends-again-api.herokuapp.com/auth/login", userCredential);
-    dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+    const res = await axios.post("/api/auth/login", userCredential);
+    dispatch({
+      type: "LOGIN_SUCCESS",
+      payload: res.data
+    });
   } catch (err) {
-    dispatch({ type: "LOGIN_FAILURE", payload: alert(err) });
+    dispatch({
+      type: "LOGIN_FAILURE",
+      payload: alert(err)
+    });
   }
 };
-
